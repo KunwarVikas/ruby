4c95e5b076f8d322673085b2bb5c64a291925cd2
Kunwar <kunwar.nitw@gmail.com>
2020-12-29 20:35:10 +0530

    Ruby Programming

---
+++ src/file/read_and_write_file.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+#Read first 20 char of a file
+aFile = File.new("resources/input.txt", "r")
+if aFile
+   content = aFile.sysread(20)
+   puts content
+else
+   puts "Unable to open file!"
+end
+
+#Write to an existing file
+aFile = File.new("resources/input1.txt", "r+")
+if aFile
+   aFile.syswrite("ABCDEF")
+else
+   puts "Unable to open file!"
+end
+
+#IO.readlines
+arr = IO.readlines("resources/input.txt")
+puts arr[0]
+puts arr[1]
+puts arr[2]
+puts arr[3]
+puts arr[4]
