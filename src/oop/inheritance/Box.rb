ae2f96d024f0cc660038b363aae38b61e5655fc3
Kunwar <kunwar.nitw@gmail.com>
2021-01-03 19:01:42 +0530

    Ruby Programming

---
+++ src/oop/inheritance/Box.rb
@@ -0,0 +1,25 @@
+class Box
+    @wid,@hei=10,10
+    def initialize(w,h)
+        @wid, @hei = w,h
+    end
+
+    def getArea
+        @wid*@hei
+        puts "Big box area is : #@area"
+    end
+end
+
+class SubBox < Box
+
+   # method overriding
+   def getArea
+      @area = @width * @height
+      puts "Big box area is : #@area"
+   end
+end
+
+# create an object
+box = SubBox.new(10, 20)
+# print the area using overridden method.
+box.getArea()
